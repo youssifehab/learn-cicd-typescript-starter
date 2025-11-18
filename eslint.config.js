4037731919806beb8e33445b8a74a0ae713d824e
youssifehab <youssifehab218@gmail.com>
2025-11-18 10:15:14 +0200

    test broke in lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,11 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { ignores: ["dist/**"] },
   {
     files: ["src/**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: { globals: globals.browser },
   },
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
 ]);
